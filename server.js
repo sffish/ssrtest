26e2e13c5bd73179786b0d48b920cef7bd28e66b
sffish <sffish@gmail.com>
2018-12-24 16:41:53 +0800

    fix  build environment

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
+const port = parseInt(process.env.PORT, 10) || 3000
 
 // With express
-// const express = require('express')
-// app.prepare().then(() => {
-//   express().use(handler).listen(3000)
-// })
+const express = require('express')
+app.prepare().then(() => {
+  express().use(handler).listen(port)
+})
 
 // Without express
-const {createServer} = require('http')
-app.prepare().then(() => {
-  createServer(handler).listen(3000)
-})
+// const {createServer} = require('http')
+// app.prepare().then(() => {
+//   createServer(handler).listen(3000)
+// })
