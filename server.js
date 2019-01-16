4ee4b663f3ba3b03a6a0c05fd36d714e7e691325
sffish <sffish@gmail.com>
2019-01-16 17:19:39 +0800

    add assetPrefix to server.js

--- server.js
+++ server.js
@@ -3,16 +3,29 @@ const next = require('next')
 const routes = require('./routes')
 const app = next({dev: process.env.NODE_ENV !== 'production'})
 const handler = routes.getRequestHandler(app)
+const handleNextRequests = app.getRequestHandler()
 const port = parseInt(process.env.PORT, 10) || 3000
 
 // With express
-const express = require('express')
+//const express = require('express')
 app.prepare().then(() => {
-  express().use(handler).listen(port)
-})
+	const server = micro((req, res) => {
+    // Add assetPrefix support based on the hostname
+    if (req.headers.host === 'sffish.me') {
+      app.setAssetPrefix('http://sffish.me/demo/')
+    } else {
+      app.setAssetPrefix('')
+    }
 
-// Without express
-// const {createServer} = require('http')
-// app.prepare().then(() => {
-//   createServer(handler).listen(3000)
-// })
+    handleNextRequests(req, res)
+  })
+
+  server.listen(port, (err) => {
+    if (err) {
+      throw err
+    }
+
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+  //express().use(handler).listen(port)
+})
