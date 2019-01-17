deedcac4909f9691c5096fdbc7f3473888b7f14d
sffish.me <sffish@gmail.com>
2019-01-17 08:48:39 +0000

    fix route

--- routes.js
+++ routes.js
@@ -1,12 +1,5 @@
 const routes = module.exports = require('next-routes')()
-const getPath = (path)=>{
-	if (process.env.NODE_ENV === 'production')
-		return `/demo${path}`
-	else 
-		return path
-}
-
 routes
 .add('about')
-.add('index', getPath('/') )
-.add('post', getPath('/post/:key') )
+.add('index', '/')
+.add('post', '/post/:key')
