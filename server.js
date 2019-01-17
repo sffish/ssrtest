1c2d71160abc486d4ee6a36fd80e18959e3292e1
sffish <sffish@gmail.com>
2019-01-17 15:07:27 +0800

    change assetprefix  due to reverse proxy

--- server.js
+++ server.js
@@ -6,26 +6,36 @@ const handler = routes.getRequestHandler(app)
 const handleNextRequests = app.getRequestHandler()
 const port = parseInt(process.env.PORT, 10) || 3000
 
-// With express
-//const express = require('express')
+const express = require('express')
 app.prepare().then(() => {
-	const server = micro((req, res) => {
-    // Add assetPrefix support based on the hostname
+	// const server = micro((req, res) => {
+ //    // Add assetPrefix support based on the hostname
+ //    if (req.headers.host === 'sffish.me') {
+ //      app.setAssetPrefix('http://sffish.me/demo/')
+ //    } else {
+ //      app.setAssetPrefix('')
+ //    }
+
+ //    handleNextRequests(req, res)
+ //  })
+
+ //  server.listen(port, (err) => {
+ //    if (err) {
+ //      throw err
+ //    }
+
+ //    console.log(`> Ready on http://localhost:${port}`)
+ //  })
+  express()
+  .use((req, res) =>{
+		//Add assetPrefix support based on the hostname
     if (req.headers.host === 'sffish.me') {
       app.setAssetPrefix('http://sffish.me/demo/')
     } else {
       app.setAssetPrefix('')
     }
-
     handleNextRequests(req, res)
   })
-
-  server.listen(port, (err) => {
-    if (err) {
-      throw err
-    }
-
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-  //express().use(handler).listen(port)
+  .use(handler)
+  .listen(port)
 })
